c8022066ae1aa1faf6475f3c22565dfa2fb9e1fe
HK.L <HK.L@naver.com>
2013-07-18 18:39:03 +0900

    과제올려요-향기-

---
+++ kankyung/kaoni/lhk/kaoniday01/test2/test2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    class Hello
+    {
+        public static int month = 7;
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine(month);
+        }
+    }
+}
---
+++ kankyung/kaoni/lhk/kaoniday01/test5/test5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hellow, world";  
+            Console.WriteLine(greeting);  
+            string greeting2 = Console.ReadLine();  
+            string greeting3 = Console.ReadLine();  
+            Console.WriteLine(greeting2);  
+            Console.WriteLine(greeting3);  
+        }
+    }
+}
