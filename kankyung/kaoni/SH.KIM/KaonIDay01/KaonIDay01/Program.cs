d45247f5e7b24c6691c58f561c6cde76b0a3f433
KSH <KSH@naver.com>
2013-07-18 18:27:55 +0900

    김승한 첫날 과제

---
+++ kankyung/kaoni/SH.KIM/KaonIDay01/KaonIDay01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaonIDay01
+{
+    class Program
+    {
+        public static int month = 7;
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(month.ToString());
+            Console.WriteLine("Hello, World");
+            String greeting = "HI, World";
+            Console.WriteLine(greeting);
+        
+        }
+    }
+}
