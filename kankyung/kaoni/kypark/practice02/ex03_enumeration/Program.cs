a51c505619e72d19c71ee9a2f36fbaf1f1f64659
kypark <your.kypark@example.com>
2013-07-19 16:35:51 +0900

    7/19 복습 내용일입니다.

---
+++ kankyung/kaoni/kypark/practice02/ex03_enumeration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03_enumeration
+{
+    enum Color { Red = 10, Green = 100, Blue = 200 }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color colorPalette = Color.Red;
+            Console.WriteLine(colorPalette);
+        }
+    }
+}
