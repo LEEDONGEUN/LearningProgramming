4b9d2f03186900936af38e22205dc92bd483883d
kypark <your.kypark@example.com>
2013-07-19 17:10:37 +0900

    7/19 복습내용입니다.

--- kankyung/kaoni/kypark/practice02/ex03_enumeration/Program.cs
+++ kankyung/kaoni/kypark/practice02/ex03_enumeration/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ex03_enumeration
 {
-    enum Color { Red = 10, Green = 100, Blue = 200 }
+    enum Color { Red = 10, Green = 100, Blue = 200, Yellow = 300 }
 
     class Program
     {
@@ -14,6 +14,12 @@ namespace ex03_enumeration
         {
             Color colorPalette = Color.Red;
             Console.WriteLine(colorPalette);
+            Color colorPalette1 = Color.Blue;
+            Console.WriteLine(colorPalette1);
+            colorPalette = Color.Green;
+            Console.WriteLine(colorPalette);
+            colorPalette1 = Color.Yellow;
+            Console.WriteLine(colorPalette1);
         }
     }
 }
