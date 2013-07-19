b0a83e2550792f7c6bf5580ff387a539b9ef9b9f
onmoving <onmoving@gmail.com>
2013-07-19 13:00:54 +0900

    minor update ofr Exception test

--- kankyung/kaoni/jin/FormattingTest/ExceptionTest/Program.cs
+++ kankyung/kaoni/jin/FormattingTest/ExceptionTest/Program.cs
@@ -10,18 +10,14 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            //try
-            //{
+            try
+            {
                 Console.WriteLine(args[0]);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("Exception at > {0}", e.StackTrace);
-            //}
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception at > {0}", e.StackTrace);
+            }
         }
-
-//        Exception at >    위치: ExceptionTest.Program.Main(String[] args) 파일 d:\hk\Lea
-//rningProgramming\kankyung\kaoni\jin\FormattingTest\ExceptionTest\Program.cs:줄 1
-//5
     }
 }
