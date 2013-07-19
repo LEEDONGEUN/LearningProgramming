0e443cc4615c74dc240349c23ad3318687e48683
onmoving <onmoving@gmail.com>
2013-07-19 11:53:22 +0900

    add FormattingTest project

---
+++ kankyung/kaoni/jin/FormattingTest/ExceptionTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+                Console.WriteLine(args[0]);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Exception at > {0}", e.StackTrace);
+            //}
+        }
+
+//        Exception at >    위치: ExceptionTest.Program.Main(String[] args) 파일 d:\hk\Lea
+//rningProgramming\kankyung\kaoni\jin\FormattingTest\ExceptionTest\Program.cs:줄 1
+//5
+    }
+}
