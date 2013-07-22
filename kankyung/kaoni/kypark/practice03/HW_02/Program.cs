50ed6d43a2bb4a99f219149155b36883be95d396
kypark <your.kypark@example.com>
2013-07-22 17:25:44 +0900

    7월 22일 연습 및 과제입니다.

--- kankyung/kaoni/kypark/practice03/HW_02/Program.cs
+++ kankyung/kaoni/kypark/practice03/HW_02/Program.cs
@@ -14,56 +14,58 @@ namespace HW_02
             for (i = 0; i >= 0; i++)
             {
                 string game = Console.ReadLine();
-                if (game == "가위")
-                {
-                    if ((i % 3) + 1 == 1)
-                    {
-                        Console.WriteLine("당신이 이겼습니다.");
-                    }
-                    else if ((i % 3) + 1 == 2)
-                    {
-                        Console.WriteLine("비겼습니다.");
-                    }
-                    else if ((i % 3) + 1 == 3)
-                    {
-                        Console.WriteLine("당신이 졌습니다.");
-                    }
-                }
-                else if (game == "바위")
-                {
-                    if ((i % 3) + 1 == 1)
-                    {
-                        Console.WriteLine("비겼습니다.");
-                    }
-                    else if ((i % 3) + 1 == 2)
-                    {
-                        Console.WriteLine("당신이 졌습니다.");
-                    }
-                    else if ((i % 3) + 1 == 3)
-                    {
-                        Console.WriteLine("당신이 이겼습니다.");
-                    }
-                }
-                else if (game == "보")
-                {
-                    if ((i % 3) + 1 == 1)
-                    {
-                        Console.WriteLine("당신이 졌습니다.");
-                    }
-                    else if ((i % 3) + 1 == 2)
-                    {
-                        Console.WriteLine("비겼습니다.");
-                    }
-                    else if ((i % 3) + 1 == 3)
-                    {
-                        Console.WriteLine("당신이 이겼습니다.");
-                    }
-                }
-                else if (game == "Quit")
+                if (game == "그만")
                 {
                     i = -2;
                 }
-
+                else
+                {
+                    if ((i % 3) + 1 == 1)
+                    {
+                        if (game == "바위")
+                        {
+                            Console.WriteLine("비겼습니다.");
+                        }
+                        if (game == "가위")
+                        {
+                            Console.WriteLine("당신이 졌습니다.");
+                        }
+                        if (game == "보")
+                        {
+                            Console.WriteLine("당신이 이겼습니다.");
+                        }
+                    }
+                    else if ((i % 3) + 1 == 2)
+                    {
+                        if (game == "바위")
+                        {
+                            Console.WriteLine("당신이 이겼습니다.");
+                        }
+                        if (game == "가위")
+                        {
+                            Console.WriteLine("비겼습니다.");
+                        }
+                        if (game == "보")
+                        {
+                            Console.WriteLine("당신이 졌습니다.");
+                        }
+                    }
+                    else if ((i % 3) + 1 == 3)
+                    {
+                        if (game == "바위")
+                        {
+                            Console.WriteLine("당신이 졌습니다.");
+                        }
+                        if (game == "가위")
+                        {
+                            Console.WriteLine("당신이 이겼습니다.");
+                        }
+                        if (game == "보")
+                        {
+                            Console.WriteLine("비겼습니다.");
+                        }
+                    }
+                }
             }
         }
     }
