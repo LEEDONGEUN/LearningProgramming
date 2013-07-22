ec2794b98ee254536aec1dfacf81622cbd7846c9
Kim Min Wook <alsdnr27@gmail.com>
2013-07-22 18:08:38 +0900

    7월22일 3번째 과제( 구구단 / 가위바위보-예외처리-)

--- kankyung/kaoni/mw.kim/KaoniDay03/RockPaperScissors/Program.cs
+++ kankyung/kaoni/mw.kim/KaoniDay03/RockPaperScissors/Program.cs
@@ -8,16 +8,31 @@ namespace RockPaperScissors
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Result();
+            try
+            {
+                Result();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("┌────────────┐");
+                Console.WriteLine("│ 잘못입력하셨습니다.    │");
+                Console.WriteLine("│ 다시 입력하세요.       │");
+                Console.WriteLine("└────────────┘");
+                Main();
+            }
         }
 
         static int Select()
         {
-            
-            Console.WriteLine(" 1.가위 2.바위 3.보 4.종료");
+            Console.WriteLine("┌────┐");
+            Console.WriteLine("│ 1.가위 │");
+            Console.WriteLine("│ 2.바위 │");
+            Console.WriteLine("│ 3. 보  │");
+            Console.WriteLine("├────┤");
+            Console.WriteLine("│ 4.종료 │");
+            Console.WriteLine("└────┘");
             Console.Write(" 무엇을 내겠습니까? ");
             string a = Console.ReadLine();
             int Num = int.Parse(a);
@@ -38,75 +53,98 @@ namespace RockPaperScissors
 
                 if (Num == 4)
                 {
+                    Console.WriteLine("┌───────┐");
+                    Console.WriteLine("│ 종료합니다.  │");
+                    Console.WriteLine("└───────┘");
                     break;
                 }
                 else if ((Num == 1) && (i == 3))
                 {
                     Console.WriteLine("플레이어 : 가위");
                     Console.WriteLine("컴 퓨 터 :  보");
-                    Console.WriteLine("플레이어가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 플레이어 승리.   │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 1) && (i == 1))
                 {
                     Console.WriteLine("플레이어 : 가위");
                     Console.WriteLine("컴 퓨 터 : 가위");
-                    Console.WriteLine("비겼습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 비겼습니다.      │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 1) && (i == 2))
                 {
                     Console.WriteLine("플레이어 : 가위");
                     Console.WriteLine("컴 퓨 터 : 바위");
-                    Console.WriteLine("컴퓨터가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 컴퓨터 승리.     │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 2) && (i == 1))
                 {
                     Console.WriteLine("플레이어 : 바위");
                     Console.WriteLine("컴 퓨 터 : 가위");
-                    Console.WriteLine("플레이어가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 플레이어 승리.   │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 2) && (i == 2))
                 {
                     Console.WriteLine("플레이어 : 바위");
                     Console.WriteLine("컴 퓨 터 : 바위");
-                    Console.WriteLine("비겼습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 비겼습니다.      │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 2) && (i == 3))
                 {
                     Console.WriteLine("플레이어 : 바위");
                     Console.WriteLine("컴 퓨 터 :  보");
-                    Console.WriteLine("컴퓨터가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 컴퓨터 승리.     │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 3) && (i == 2))
                 {
                     Console.WriteLine("플레이어 :  보");
                     Console.WriteLine("컴 퓨 터 : 바위");
-                    Console.WriteLine("플레이어가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 플레이어 승리.   │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 3) && (i == 3))
                 {
                     Console.WriteLine("플레이어 :  보");
                     Console.WriteLine("컴 퓨 터 :  보");
-                    Console.WriteLine("비겼습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 비겼습니다.      │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else if ((Num == 3) && (i == 1))
                 {
                     Console.WriteLine("플레이어 :  보");
                     Console.WriteLine("컴 퓨 터 : 가위");
-                    Console.WriteLine("컴퓨터가 승리하였습니다.");
+                    Console.WriteLine("┌─────────┐");
+                    Console.WriteLine("│ 컴퓨터 승리.     │");
+                    Console.WriteLine("└─────────┘");
                     Console.WriteLine();
                 }
                 else
                 {
-                    Console.WriteLine("다시 입력하세요");
-                    Console.WriteLine();
+                    Console.WriteLine("┌────────────┐");
+                    Console.WriteLine("│ 잘못입력하셨습니다.    │");
+                    Console.WriteLine("│ 다시 입력하세요.       │");
+                    Console.WriteLine("└────────────┘");
                 }
             } while (true);
         }
