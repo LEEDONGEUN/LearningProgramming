fad5afccd81a5c045cd4cb2790eb8635533197ba
kypark <your.kypark@example.com>
2013-07-22 18:03:16 +0900

    7월 22일 연습 및 과제입니다.

--- kankyung/kaoni/kypark/practice03/HW_02/Program.cs
+++ kankyung/kaoni/kypark/practice03/HW_02/Program.cs
@@ -10,6 +10,7 @@ namespace HW_02
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("가위, 바위, 보, 그만 중에 하나를 입력하세요.");
             int i;
             for (i = 0; i >= 0; i++)
             {
@@ -24,45 +25,45 @@ namespace HW_02
                     {
                         if (game == "바위")
                         {
-                            Console.WriteLine("비겼습니다.");
+                            Console.WriteLine("컴퓨터 : 바위 - 비겼습니다.");
                         }
                         if (game == "가위")
                         {
-                            Console.WriteLine("당신이 졌습니다.");
+                            Console.WriteLine("컴퓨터 : 바위 - 당신이 졌습니다.");
                         }
                         if (game == "보")
                         {
-                            Console.WriteLine("당신이 이겼습니다.");
+                            Console.WriteLine("컴퓨터 : 바위 - 당신이 이겼습니다.");
                         }
                     }
                     else if ((i % 3) + 1 == 2)
                     {
                         if (game == "바위")
                         {
-                            Console.WriteLine("당신이 이겼습니다.");
+                            Console.WriteLine("컴퓨터 : 가위 - 당신이 이겼습니다.");
                         }
                         if (game == "가위")
                         {
-                            Console.WriteLine("비겼습니다.");
+                            Console.WriteLine("컴퓨터 : 가위 - 비겼습니다.");
                         }
                         if (game == "보")
                         {
-                            Console.WriteLine("당신이 졌습니다.");
+                            Console.WriteLine("컴퓨터 : 가위 - 당신이 졌습니다.");
                         }
                     }
                     else if ((i % 3) + 1 == 3)
                     {
                         if (game == "바위")
                         {
-                            Console.WriteLine("당신이 졌습니다.");
+                            Console.WriteLine("컴퓨터 : 보 - 당신이 졌습니다.");
                         }
                         if (game == "가위")
                         {
-                            Console.WriteLine("당신이 이겼습니다.");
+                            Console.WriteLine("컴퓨터 : 보 - 당신이 이겼습니다.");
                         }
                         if (game == "보")
                         {
-                            Console.WriteLine("비겼습니다.");
+                            Console.WriteLine("컴퓨터 : 보 - 비겼습니다.");
                         }
                     }
                 }
