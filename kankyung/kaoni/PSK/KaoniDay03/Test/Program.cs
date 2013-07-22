2b061b63f2b3d2e25ad18120c8e40f410ca1c6bd
psk <mafuyu11@naver.com>
2013-07-22 17:21:40 +0900

    2013.07.22 후냐~~~

---
+++ kankyung/kaoni/PSK/KaoniDay03/Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuGuDan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Setting();
+
+            /*for (int j = 2; j < 10; j++)
+            {
+                Console.WriteLine("\n\n{0}단", j);
+
+                for (int i = 1; i < 10; i++)
+                {
+                    Console.WriteLine("{0} X {1} = {2}", j, i, st.Gugudan(j, i));
+                } 
+            }*/ // 나열
+
+            var j = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (j < 2 || j >= 10) return;
+                Console.WriteLine("{0} X {1} = {2}", j, i, st.Gugudan(j, i));
+            } 
+        }
+    }
+
+    class Setting
+    {
+        public Setting() { }
+
+        public int Gugudan(int i, int j)
+        {
+            return i * j;
+        }
+    }
+}
