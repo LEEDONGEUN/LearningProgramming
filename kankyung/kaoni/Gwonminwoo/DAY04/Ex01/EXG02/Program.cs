898e39d38c4bf2487625dd8f9be93aefba7efe1f
happymanu <happymanu@nate.com>
2013-07-23 19:37:15 +0900

    2013.07.23

---
+++ kankyung/kaoni/Gwonminwoo/DAY04/Ex01/EXG02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EXG02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <int>que = new Queue<int>();
+             que.Enqueue(1);
+             que.Enqueue(2);
+             que.Enqueue(3);
+             que.Enqueue(4);
+             que.Enqueue(5);
+             while (que.Count > 0)
+                 Console.WriteLine(que.Dequeue());
+        }
+        }
+
+    }
+
