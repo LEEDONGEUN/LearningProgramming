490fb4eb9e8421515322f498c805ba72d5b72d34
KJO <jauo6409@nate.com>
2013-07-24 16:01:38 +0900

    2013-07-23 권종오

---
+++ kankyung/kaoni/jauo/Kaoniday04/07_23/07_23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_23_Arry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort method
+            int[] data = { 4, 6, 3, 8, 9, 3 };
+            System.Array.Sort(data);
+
+            //Clear
+            int[] data1 = { 4, 6, 3, 8, 9, 3, };
+            System.Array.Clear(data1, 3, 2);
+
+            //clone method
+            int[] data2 = { 4, 6, 3, 8, 9, 3 };
+            int[] clone = (int[])data2.Clone();
+            data2[2] = 100;
+            data2[3] = 100;
+
+            //GetLenght 
+            int[,] data3 = { { 0, 1, 2, 3 }, { 4, 5, 6, 7, } };
+            int dim0 = data3.GetLength(0); // ==2
+            int dim1 = data3.GetLength(1); // ==4
+
+            //indexOf
+            int[] data4 = { 4, 6, 3, 8, 9, 3, };
+            int where = System.Array.IndexOf(data, 9); // ==4
+        }
+    }
+}
---
+++ kankyung/kaoni/jauo/Kaoniday04/07_23_Parameter/07_23_Parameter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_23_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static string Add(string a, string b)
+        {
+            return string.Concat(a, b);
+        }
+        static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        //static int Add(int n1, int n2)
+        //{
+        //    return 3 + 4;
+        //}
+        static int Add(params int [] intArr)
+        {
+            int retValue = 0;
+            foreach (int item in intArr)
+            {
+                retValue += item;
+              //  retValue = retvalue + item;
+            }
+
+            return retValue;
+        }
+
+    }
+}
